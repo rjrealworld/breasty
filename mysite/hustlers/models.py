49bf50fad5d34e4ef7885d510ecbff25bb179b93
shruti463 <65339232+shruti463@users.noreply.github.com>
2020-11-21 01:30:06 +0530

    Update models.py

--- mysite/hustlers/models.py
+++ mysite/hustlers/models.py
@@ -4,5 +4,8 @@ class Task(models.Model):
     date=models.DateTimeField(auto_now_add=False)
     task=models.CharField(max_length=230)
     due = models.DateTimeField(auto_now_add=False , blank=True, null=True)
+    
+    def __str__(self):
+        return self.task
 
 
