62bf80265c85e16db32bc82b3028f71430442bf2
shruti463 <65339232+shruti463@users.noreply.github.com>
2020-11-20 14:07:12 +0530

    Add files via upload

---
+++ mysite/hustlers/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Task(models.Model):
+    date=models.DateTimeField(auto_now_add=False)
+    task=models.CharField(max_length=230)
+    due = models.DateTimeField(auto_now_add=False , blank=True, null=True)
+
+
